f514a2b3ac220fb90657904482bd10e96e88bd53
mariajco <141979755+mariajco@users.noreply.github.com>
2023-10-23 21:07:04 -0500

    Felino.java

---
+++ Felino.java
@@ -0,0 +1,25 @@
+public class Felino extends Animal {
+    public Felino(String sonidos, String alimentos, String habitat, String nombreCientifico) {
+        super(sonidos, alimentos, habitat, nombreCientifico);
+    }
+
+    @Override
+    public String getNombreCientifico() {
+        return super.getNombreCientifico();
+    }
+
+    @Override
+    public String getSonido() {
+        return super.getSonido();
+    }
+
+    @Override
+    public String getAlimentos() {
+        return super.getAlimentos();
+    }
+
+    @Override
+    public String getHabitat() {
+        return super.getHabitat();
+    }
+}
